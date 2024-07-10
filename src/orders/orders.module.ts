56eb0406fb15d2efb6811b002249c7e80f0bd60f
hongantran <hongan.tran@beme.vn>
2024-07-10 21:25:03 +0700

    fix mail

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { PrismaService } from '../prisma.service';
-
+import { MailService } from 'src/mail/mail.service';
 @Module({
   controllers: [OrdersController],
-  providers: [PrismaService,OrdersService],
+  providers: [MailService,PrismaService,OrdersService],
 })
 export class OrdersModule {}
