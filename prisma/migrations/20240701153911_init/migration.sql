334867118fb27f6d9cece6c72219413ed17af10f
root <root@srv508833.hstgr.cloud>
2024-07-16 17:52:56 +0000

    backup

--- prisma/migrations/20240701153911_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "Article" ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-ADD COLUMN     "updated_at" TIMESTAMP(3);
-
--- AlterTable
-ALTER TABLE "Store" ALTER COLUMN "phone" DROP NOT NULL;
-
--- DropEnum
-DROP TYPE "ProductStatus";
