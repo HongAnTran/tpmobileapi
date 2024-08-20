8be6970af448261e0f5d116a950c05c29d623aab
hongantran <hongan.tran@beme.vn>
2024-08-21 01:15:16 +0700

    register cronjob

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -3,8 +3,9 @@ import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { PrismaService } from '../prisma.service';
 import { MailService } from 'src/mail/mail.service';
+import { OrderCleanupService } from 'src/common/services/orderCleanup.service';
 @Module({
   controllers: [OrdersController],
-  providers: [MailService,PrismaService,OrdersService],
+  providers: [MailService,PrismaService,OrdersService , OrderCleanupService],
 })
 export class OrdersModule {}
