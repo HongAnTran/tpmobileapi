ac532cf48c19df9911738fbf5a648400fa2478cf
hongantran <hongan.tran@beme.vn>
2024-10-16 16:19:14 +0700

    fix

--- src/product-image/product-image.module.ts
+++ src/product-image/product-image.module.ts
@@ -3,8 +3,9 @@ import { ProductImageService } from './product-image.service';
 import { ProductImageController } from './product-image.controller';
 import { PrismaService } from 'src/prisma.service';
 import { StaticService } from 'src/static/static.service';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 @Module({
   controllers: [ProductImageController],
-  providers: [ProductImageService,PrismaService ,StaticService],
+  providers: [ProductImageService,PrismaService ,StaticService , CloudinaryService],
 })
 export class ProductImageModule {}
