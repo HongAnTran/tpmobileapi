5282a93ec57549f2f2f1f9aca2265684a0534a51
HongAnTran <tranhongankrn.2001@gmail.com>
2024-11-25 19:25:44 +0700

    up

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -2,4 +2,4 @@ import { Injectable, OnModuleInit } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {}
+export class PrismaService extends PrismaClient {}
