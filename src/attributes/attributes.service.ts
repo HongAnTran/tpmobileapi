529b687f2e5e85cf1b32da5d7dedf9a28b0da874
HongAnTran <tranhongankrn.2001@gmail.com>
2025-04-14 18:10:10 +0700

    temp

--- src/attributes/attributes.service.ts
+++ src/attributes/attributes.service.ts
@@ -49,6 +49,6 @@ export class AttributesService {
 
 
   async getProductAttributeValue(){
-    return this.prisma.productAttributeValue.findMany();
+    return this.prisma.productAttributes.findMany();
   }
 }
